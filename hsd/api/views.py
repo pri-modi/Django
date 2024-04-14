4dfade67a690e5aabfffd0a98ecde5893f1c5f5e
pri-modi <89729458+pri-modi@users.noreply.github.com>
2024-04-14 17:05:16 +0530

    Set up the views.py file

--- hsd/api/views.py
+++ hsd/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import speechDetection
+from .serializers import speechDetectionSerializer
 
 # Create your views here.
+
+class speechDetectionListCreate(generics.ListCreateAPIView):
+    queryset = speechDetection.objects.all()
+    serializer_class = speechDetectionSerializer
