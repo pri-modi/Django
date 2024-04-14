4fda9ff5fc244c35382da98c8f625f4daf809f90
pri-modi <89729458+pri-modi@users.noreply.github.com>
2024-04-14 16:46:49 +0530

    Added all the fie;ds that are required in the model

--- hsd/api/models.py
+++ hsd/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class speechDetection(models.Model):
+    # define all the fields that your model is supposed to have
+    text = models.CharField(max_length=10000)
