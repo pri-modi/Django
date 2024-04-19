d534e3acf61457879e98f11096b96299c8502404
pri-modi <89729458+pri-modi@users.noreply.github.com>
2024-04-19 19:11:58 +0530

    Created the basic layout of the project including api

--- hsd/api/models.py
+++ hsd/api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class speechDetection(models.Model):
     # define all the fields that your model is supposed to have
     text = models.CharField(max_length=10000)
-    published_date = models.DateTimeField(auto_now_add=True)
+    # published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.text
