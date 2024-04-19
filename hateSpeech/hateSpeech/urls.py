ad86e1d639761c53b77540ad50e8a511d903a79f
pri-modi <89729458+pri-modi@users.noreply.github.com>
2024-04-19 22:08:57 +0530

    Created end points by setting urls.py files, and mentioned the fields that will be required in the model in models.py

--- hateSpeech/hateSpeech/urls.py
+++ hateSpeech/hateSpeech/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from hateSpeech import MyAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('api/', MyAPI.site.urls),
+    path('', include("MyAPI.urls")),
 ]
