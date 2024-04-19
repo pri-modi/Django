41a22289e78303180feb4b92610c590038d7647a
pri-modi <89729458+pri-modi@users.noreply.github.com>
2024-04-19 22:21:37 +0530

    Created serializers.py in MyAPI

---
+++ hateSpeech/MyAPI/serializers.py
@@ -0,0 +1,10 @@
+#Specify the class that will convert the model into json compatible data
+# Handle requests and convert them into json file in the background
+from rest_framework import serializers
+from rest_framework import detection
+from .models import detection
+
+class detectionSerializers(serializers.ModelSerializers):
+    class Meta:
+        model = detection
+        fields = '__all__'
